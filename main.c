f6ca8962ae13a238e8c77e4c74db9920b97cc8cc
Jeremy Stott <jeremy@stott.co.nz>
2017-03-14 23:57:27 +1300

    Improved example output text.

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include "commithash.h"
 
 int main(void) {
-    printf("The current version is %s\n", version.string);
+    printf("The current version: %s\n", version.string);
+    printf("Major version: %d\n", version.major);
+    printf("Minor version: %d\n", version.minor);
+    printf("Patch version: %d\n", version.patch);
+    printf("Commits since last tag: %d\n", version.commits);
+    printf("Latest commit hash: %s\n", version.hash);
+    if (version.dirty) {
+        printf("The working tree was: dirty\n");
+    } else {
+        printf("The working tree was: clean\n");
+    }
     return 0;
 }
